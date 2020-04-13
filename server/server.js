b024c6436ee0abb478a1ca0ca464f539261261d1
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-04-12 21:23:53 -0500

    grabbed top 25 popular reddit articles and push them into an array

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const path = require('path');
 const fs = require('fs');
 const request = require('request');
+const rp = require('request-promise');
 
-let dataPath = path.join(__dirname, '../chirps.json');
+let dataPath = path.join(__dirname, '../reddit.js');
 
-fs.readFile('chirps.json', (err, data) => {
-  if (err) console.log(err);
-  let chirpers = JSON.parse(data);
-  console.log(chirpers);
+rp('https://reddit.com/r/popular.json', (err, res, body) => {
+  if(err) console.log(err);
+
+  let articles = [];
+
+  JSON.parse(body).data.children.forEach(item => {
+    // fs.appendFileSync(dataPath,item.data.title + '\n');
+    // fs.appendFileSync(dataPath,item.data.url + '\n');
+    // fs.appendFileSync(dataPath,item.data.author + '\n');
+    articles.push(fs.appendFileSync(dataPath,item.data.url + '\n'))
+    console.log(articles);
+  });
+  
+
 });
